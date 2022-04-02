f5eb0b339ad16df6b8ae3213b77657b07a515423
boyanborislavov <b.borislavov@infoleven.com>
2022-04-02 11:25:06 +0300

    fixing state bugs

--- src/components/todo-form/todo-form.jsx
+++ src/components/todo-form/todo-form.jsx
@@ -1,14 +1,21 @@
-import * as React from 'react';
+import React, { useState, useContext } from 'react';
 import { TodosContext } from '../../todo-context';
 import './todo-form.scss';
 
 export const TodoForm = () => {
-  const { todos, setTodos } = React.useContext(TodosContext);
-  const [task, setTask] = React.useState('');
+  const { todos, setTodos } = useContext(TodosContext);
+  const [task, setTask] = useState('');
 
-  const handleAddTodo = () => {
-    // Fin an ability to add new task
-  };
+  // 1. Handle input task to become todo
+  // 2.We need to check number ot todos so we can calculate new id
+  // 3.Clearing the input state after adding
+  // 4. I used ternary you can use ifs
+  const handleAddTodo = () => (task ? setTodos([...todos,
+    {
+      id: todos.length > 0 ? todos[todos.length - 1].id + 1 : 0,
+      label: task,
+      checked: false,
+    }]) && setTask('') : null);
 
   const handleKeyUp = (e) => {
     if (e.keyCode === 13) {
